f59df428c355b7c081ac799c1f91564c75d1d5fc
Saranyajothy <saranyajothikumar12@gmail.com>
2022-02-22 23:10:04 +0000

    add accepting answers true or false, incorrect and correct answer

--- assets/js/human.js
+++ assets/js/human.js
@@ -53,3 +53,65 @@ let questions =[
         answer: A, 
     },
 ]
+const score_point = 100
+const max_questions = 5
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > max_questions) {
+        localStorage.setItem('mostRecentScore', score)
+        return window.location.assign('/end.html')
+    }
+}
+
+questionCounter++ 
+progressText.innerText = `Question ${questionCounter} of ${max-questions}`
+progressBarFull.style.width = `${(questionCounter/max_questions) * 100}%`
+
+constquestionIndex = Math.floor(Math.random() * availableQuestions.length)
+currentQuestion = availableQuestions[questionIndex]
+question.innerText = currentQuestion.question
+
+choices.forEach(choice => {
+    const number = choice.dataset['number']
+    choice.innerText = currentQuestion['choice' + number]
+})
+
+availableQuestions.splice(questionsIndex, 1)
+
+acceptingAnswers = true
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+        acceptingAnswers = false
+
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'  
+          
+        if(classToApply === 'correct') {
+            incrementScore(score_point)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() =>{
+            selectedChoice.parentElement.classList.remove(classToApply)
+        }, 100)
+    })
+
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+startGame()
