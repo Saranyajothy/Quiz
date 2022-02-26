ee09e42c308a604d4b2d878aa59ddfd6e829dd5a
Saranyajothy <saranyajothikumar12@gmail.com>
2022-02-26 01:56:27 +0000

    structure and style the food category

--- assets/js/human.js
+++ assets/js/human.js
@@ -70,26 +70,7 @@ getNewQuestion = () => {
         localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('end-game.html')
-    }
-
-    
-// questionCounter++ 
-// progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS }`
-// progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS ) * 100}%`
-
-// const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-// currentQuestion = availableQuestions[questionsIndex]
-// question.innerText = currentQuestion.question
-
-// choices.forEach(choice => {
-//     const number = choice.dataset['number']
-//     choice.innerText = currentQuestion['choice' + number]
-// })
-
-// availableQuestions.splice(questionsIndex, 1)
-
-// acceptingAnswers = true
-// }
+}
 
 questionCounter++ 
 progressText.innerText = `Question ${questionCounter} of ${MAXIMUM_QUESTIONS}`
@@ -137,35 +118,5 @@ incrementScore = num => {
     score +=num
     scoreText.innerText = score
 }
-
-// function optionSelected(answer)
-//     clearInterval(counter);
-//     clearInterval(counterLine);
-//     let userAnswer = answer.textContent;
-//     let correcAnswer =question[que_count].answer;
-//     const allOptions = option_list.children.lenght;
-
-//     if(userAnswer ==correcAnswer) {
-//         userScore += 1;
-//         answer.classList.add("correct");
-//         answer.insertAdjacentHTML("beforeend")
-//         console.log("Correct Answer");
-//         console.log("Your correct answer = " +userScore);
-//     }else {
-//         answer.classList.add("correct");
-//         answer.insertAdjacentHTML("beforeend")
-//         console.log("Wrong Answer");
-
-//         for(i=0; i < allOptions; i++){
-//             if(option_list.children[i].textContent == correctAnswer) {
-//                 option_list.children[i].setAttribute("class", "option correct");
-//                 option_list.children[i].insertAdjacentHTML("beforeend")
-
-//                 console.log("Auto selected correct answer.");
-//             }
-//         }
-
-       
-
 
 startGame();
