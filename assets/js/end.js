c611f1f6ab1b847c6025c6f0ab92e6294d63a765
Saranyajothy <saranyajothikumar12@gmail.com>
2022-03-02 20:49:57 +0000

    rearrange and add  the new code in end-game html,end.js

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,37 +1,81 @@
-// let user_score = sessionStorage.getItem("score");
+
+const quizScore = document.getElementById('quizScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 50;
+
+quizScore.innerText = mostRecentScore;
+
+// username.addEventListener('keyup', () => {
+//     saveScoreBtn.disabled = !username.value;
+// });
+
+// saveHighScore = (e) => {
+//     e.preventDefault();
+
+//     const score = {
+//         score: mostRecentScore,
+//     };
+//     highScores.push(score);
+//     highScores.sort((a, b) => b.score - a.score);
+//     highScores.splice(50);
+
+//     localStorage.setItem('highScores', JSON.stringify(highScores));
+//     window.location.assign('/');
+// };
+
+
+
+
+
+// const username = document.getElementsByClassName('username');
+// const userScore = localStorage.getItem('mostRecentScore');
+
+// const userScore = JSON.parse(localStorage.getItem('userScore')) || [];
+
+// const MAX_HIGH_SCORES = 50;
+
+// userScore.innerText = mostRecentScore;
  
-// document.querySelector("span.score").innerHTML = user_score;
+// document.querySelector("span.score").innerHTML = userScore;
 
- console.log('hello');
+// saveuserScore = (e) => {
+//     e.preventDefault();
 
-const result_box = document.querySelector(".result_box");
-const scoreText= document.querySelector('.scoreText');
-const MAX_HIGH_SCORES = 50
+// const score = {
+//     score: mostRecentScore,
+// };
+// userScore.push(score);
+// userScore.sort((a, b) => b.score - a.score);
+// userScore.splice(50);
 
-let score = 0
+// localStorage.setItem('userScore', JSON.stringify(userScore));
+// window.location.assign('/');
+// };
 
-function showResult(){
-    const scoreText = result_box.querySelector(".scoreText");
-if (score > 30) { 
-    let scoreTag = '<span>Congrats! , Your score <p>'+ score +'</p> out of <p>'+ question.length +'</p> </span>';
-    scoreText.innerHTML = scoreTag;
-    }
-else if(score > 10) {
-    let scoreTag = '<span>Nice , Your score <p>'+ score +'</p> out of <p>'+ question.length +'</p> </span>';
-    scoreText.innerHTML = scoreTag;
-    }
-else{
-    let scoreTag = '<span>Sorry , Your score <p>'+ score +'</p> out of <p>'+ question.length +'</p> </span>';
-    scoreText.innerHTML = scoreTag;
-    }
-return showResult 
-}
 
-// const highScoresList = document.getElementById("highScoresList");
-// const quizScore = JSON.parse(localStorage.getItem("quizScore")) || [];
 
-// highScoresList.innerHTML = quizScore
-//   .map(score => {
-//     return `<li class="high-score">${score.name} - ${score.score}</li>`;
-//   })
-//   .join();
+// // const result_box = document.querySelector(".result_box");
+// // const scoreText= document.querySelector('.scoreText');
+// // const MAX_HIGH_SCORES = 50
+
+// // let score = 0
+
+// // function resultBox(){
+// //     const scoreText = result_box.querySelector(".scoreText");
+// // if (score > 30) { 
+// //     let scoreTag = '<span>Congrats! , Your score <p>'+ score +'</p> out of <p>'+ question.length +'</p> </span>';
+// //     scoreText.innerHTML = scoreTag;
+// //     }
+// // else if(score > 10) {
+// //     let scoreTag = '<span>Nice , Your score <p>'+ score +'</p> out of <p>'+ question.length +'</p> </span>';
+// //     scoreText.innerHTML = scoreTag;
+// //     }
+// // else{
+// //     let scoreTag = '<span>Sorry , Your score <p>'+ score +'</p> out of <p>'+ question.length +'</p> </span>';
+// //     scoreText.innerHTML = scoreTag;
+// //     }
+
+// // }
--- assets/js/human.js
+++ assets/js/human.js
@@ -1,32 +1,31 @@
 const question = document.getElementById('question');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
-const choices =Array.from(document.querySelectorAll('.choice-text'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.getElementById('progressText');
 
 // console.log('hello');
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
-let questionCounter = 0 
+let questionCounter = 0
 let availableQuestions = []
 
-let questions =[
-    {
+let questions = [{
         question: "Where in the body does most of digestion take place?",
         choice1: "Small Intestine",
         choice2: "Stomach",
         choice3: "Mouth",
         choice4: "Large Intestine",
-        answer: 1, 
-        },
-{
+        answer: 1,
+    },
+    {
         question: "Where in the body are new blood cells made?",
         choice1: "Liver",
         choice2: "Brain",
         choice3: "Bone marrow",
         choice4: "Heart",
-        answer: 3, 
+        answer: 3,
     },
 
     {
@@ -35,7 +34,7 @@ let questions =[
         choice2: "Saddle",
         choice3: "Glide",
         choice4: "Ball and socket",
-        answer: 2, 
+        answer: 2,
     },
     {
         question: "What is the heaviest organ in the human body?",
@@ -43,7 +42,7 @@ let questions =[
         choice2: "Brain",
         choice3: "Liver",
         choice4: "Skin",
-        answer: 4, 
+        answer: 4,
     },
     {
         question: "What is the longest bone in the Human body?",
@@ -51,7 +50,7 @@ let questions =[
         choice2: "Collarbone",
         choice3: "Spine",
         choice4: "Shinbone",
-        answer: 1, 
+        answer: 1,
     },
 ]
 // questions and score
@@ -66,41 +65,40 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAXIMUM_QUESTIONS ) {
+    if (availableQuestions.length === 0 || questionCounter > MAXIMUM_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
-
         return window.location.assign('end-game.html')
-}
+    }
 
-questionCounter++ 
-progressText.innerText = `Question ${questionCounter} of ${MAXIMUM_QUESTIONS}`
-progressBarFull.style.width = `${(questionCounter/MAXIMUM_QUESTIONS) * 100}%`
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAXIMUM_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAXIMUM_QUESTIONS) * 100}%`
 
-const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-currentQuestion = availableQuestions[questionsIndex]
-question.innerText = currentQuestion.question
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
 
-choices.forEach(choice => {
-    const number = choice.dataset['number']
-    choice.innerText = currentQuestion['choice' + number]
-})
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
 
-availableQuestions.splice(questionsIndex, 1)
+    availableQuestions.splice(questionsIndex, 1)
 
-acceptingAnswers = true
+    acceptingAnswers = true
 }
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
+        if (!acceptingAnswers) return
 
         acceptingAnswers = false
         const selectedChoice = e.target
         const selectedAnswer = selectedChoice.dataset['number']
 
-        const classToApply = selectedAnswer == currentQuestion.answer? 'correct' : 'incorrect';  
-          
-        if(classToApply === 'correct') {
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
             incrementScore(SCORE_BONUS)
         }
 
@@ -115,7 +113,7 @@ choices.forEach(choice => {
 })
 
 incrementScore = num => {
-    score +=num
+    score += num
     scoreText.innerText = score
 }
 
